39d327c337a30f3d61ccc3c1735c709fd0363e13
Rui Shen <rui@voltdb.com>
2015-07-01 10:08:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# voltdb
+
+A Clojure library designed to test VoltDB under network partitions, using the jepsen library.
+
+## Usage
+
+TODO
+
+## License
+
+Copyright © 2015 VoltDB
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject voltdb "0.1.0-SNAPSHOT"
+  :description "Jepsen test for VoltDB"
+  :url "https://github.com/slowsoul/jepsen-voltdb"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jepsen "0.0.3"]])
